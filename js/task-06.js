c755a3e707c0a5c1dee0ce4c885fc4f256de3070
Bohdana Lozenko <102677592+beevine@users.noreply.github.com>
2022-08-10 15:40:21 +0300

    updates on task 6

--- js/task-06.js
+++ js/task-06.js
@@ -8,10 +8,9 @@
 // які ми вже додали у вихідні файли завдання.
 
 let input = document.querySelector("#validation-input");
-let quantity = input.getAttribute("data-length");
 
 input.addEventListener("blur", () => {
-    if (input.value.length === Number(quantity)) {
+    if (input.value.length == input.getAttribute("data-length")) {
         input.classList.add("valid");
         input.classList.remove("invalid");
     } else {
